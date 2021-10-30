63a2bf46406544168bbab23f326f8eb9374e4aff
Nicolas BAOUAYA-MOULOMBA <nicolasbaouaya@yahoo.fr>
2021-10-30 16:18:17 +0200

    Texte justifié sur la page d'accueil, boutons "lire plus" et bannières redimensionnées

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,19 @@ jQuery(document).ready(function() {
 	// Your custom js code goes here.
 
 });
+
+function readMoreFunction(dotsSpanName, moreSpanName, moreButtonName ) {
+	var dots = document.getElementById(dotsSpanName);
+	var moreText = document.getElementById(moreSpanName);
+	var btnText = document.getElementById(moreButtonName);
+  
+	if (dots.style.display === "none") {
+	  dots.style.display = "inline";
+	  btnText.innerHTML = "Lire plus";
+	  moreText.style.display = "none";
+	} else {
+	  dots.style.display = "none";
+	  btnText.innerHTML = "Lire moins";
+	  moreText.style.display = "inline";
+	}
+  } 
